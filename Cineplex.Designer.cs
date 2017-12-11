9b238fc64ac86b8cb6a60b908a40af961be46410
Dhruti Parekh <dhruti.parekh266@gmail.com>
2017-12-11 14:09:47 -0500

    Final Upload

---
+++ Cineplex.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace MovieFinalProject
+{
+    partial class Cineplex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.movieDataGrid = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.moviePoster = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblReleaseDate = new System.Windows.Forms.Label();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.lblDirector = new System.Windows.Forms.Label();
+            this.lblShowTime = new System.Windows.Forms.Label();
+            this.comboBoxDAY = new System.Windows.Forms.ComboBox();
+            this.showTime = new System.Windows.Forms.Label();
+            this.lblShowsTime = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.movieDataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviePoster)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // movieDataGrid
+            // 
+            this.movieDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.movieDataGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.movieDataGrid.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.movieDataGrid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.movieDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.movieDataGrid.Location = new System.Drawing.Point(12, 64);
+            this.movieDataGrid.MultiSelect = false;
+            this.movieDataGrid.Name = "movieDataGrid";
+            this.movieDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.movieDataGrid.Size = new System.Drawing.Size(699, 182);
+            this.movieDataGrid.TabIndex = 0;
+            this.movieDataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.onMovieClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(636, 252);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(555, 252);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.OliveDrab;
+            this.lblMessage.Location = new System.Drawing.Point(241, 19);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(212, 31);
+            this.lblMessage.TabIndex = 5;
+            this.lblMessage.Text = "Cineplex Movies";
+            // 
+            // moviePoster
+            // 
+            this.moviePoster.Location = new System.Drawing.Point(13, 297);
+            this.moviePoster.Name = "moviePoster";
+            this.moviePoster.Size = new System.Drawing.Size(165, 207);
+            this.moviePoster.TabIndex = 6;
+            this.moviePoster.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.lblTitle.Location = new System.Drawing.Point(198, 297);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(58, 25);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblReleaseDate
+            // 
+            this.lblReleaseDate.AutoSize = true;
+            this.lblReleaseDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReleaseDate.ForeColor = System.Drawing.Color.Black;
+            this.lblReleaseDate.Location = new System.Drawing.Point(202, 375);
+            this.lblReleaseDate.Name = "lblReleaseDate";
+            this.lblReleaseDate.Size = new System.Drawing.Size(94, 15);
+            this.lblReleaseDate.TabIndex = 8;
+            this.lblReleaseDate.Text = "Release Date";
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenre.ForeColor = System.Drawing.Color.Black;
+            this.lblGenre.Location = new System.Drawing.Point(202, 322);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(47, 13);
+            this.lblGenre.TabIndex = 9;
+            this.lblGenre.Text = "Genres";
+            // 
+            // lblDirector
+            // 
+            this.lblDirector.AutoSize = true;
+            this.lblDirector.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDirector.ForeColor = System.Drawing.Color.Black;
+            this.lblDirector.Location = new System.Drawing.Point(202, 350);
+            this.lblDirector.Name = "lblDirector";
+            this.lblDirector.Size = new System.Drawing.Size(58, 15);
+            this.lblDirector.TabIndex = 10;
+            this.lblDirector.Text = "Director";
+            // 
+            // lblShowTime
+            // 
+            this.lblShowTime.AutoSize = true;
+            this.lblShowTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.lblShowTime.ForeColor = System.Drawing.Color.Black;
+            this.lblShowTime.Location = new System.Drawing.Point(202, 402);
+            this.lblShowTime.Name = "lblShowTime";
+            this.lblShowTime.Size = new System.Drawing.Size(91, 15);
+            this.lblShowTime.TabIndex = 26;
+            this.lblShowTime.Text = "Choose Day: ";
+            // 
+            // comboBoxDAY
+            // 
+            this.comboBoxDAY.DropDownHeight = 60;
+            this.comboBoxDAY.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDAY.ForeColor = System.Drawing.Color.Teal;
+            this.comboBoxDAY.FormattingEnabled = true;
+            this.comboBoxDAY.IntegralHeight = false;
+            this.comboBoxDAY.Items.AddRange(new object[] {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"});
+            this.comboBoxDAY.Location = new System.Drawing.Point(297, 402);
+            this.comboBoxDAY.Name = "comboBoxDAY";
+            this.comboBoxDAY.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDAY.TabIndex = 25;
+            this.comboBoxDAY.SelectedIndexChanged += new System.EventHandler(this.comboBoxDAY_SelectedIndexChanged);
+            // 
+            // showTime
+            // 
+            this.showTime.AutoSize = true;
+            this.showTime.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.showTime.Location = new System.Drawing.Point(424, 402);
+            this.showTime.Name = "showTime";
+            this.showTime.Size = new System.Drawing.Size(60, 16);
+            this.showTime.TabIndex = 27;
+            this.showTime.Text = "showTime";
+            // 
+            // lblShowsTime
+            // 
+            this.lblShowsTime.AutoSize = true;
+            this.lblShowsTime.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowsTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblShowsTime.Location = new System.Drawing.Point(294, 460);
+            this.lblShowsTime.Name = "lblShowsTime";
+            this.lblShowsTime.Size = new System.Drawing.Size(0, 15);
+            this.lblShowsTime.TabIndex = 28;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(408, 252);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(141, 23);
+            this.btnUpload.TabIndex = 29;
+            this.btnUpload.Text = "Upload Poster";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // Cineplex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(723, 529);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.lblShowsTime);
+            this.Controls.Add(this.showTime);
+            this.Controls.Add(this.lblShowTime);
+            this.Controls.Add(this.comboBoxDAY);
+            this.Controls.Add(this.lblDirector);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.lblReleaseDate);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.moviePoster);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.movieDataGrid);
+            this.Name = "Cineplex";
+            this.Text = "Cineplex Movies";
+            this.Load += new System.EventHandler(this.Cineplex_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.movieDataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviePoster)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView movieDataGrid;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.PictureBox moviePoster;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblReleaseDate;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.Label lblDirector;
+        private System.Windows.Forms.Label lblShowTime;
+        private System.Windows.Forms.ComboBox comboBoxDAY;
+        private System.Windows.Forms.Label showTime;
+        private System.Windows.Forms.Label lblShowsTime;
+        private System.Windows.Forms.Button btnUpload;
+    }
+}
+
---
+++ Cineplex.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieFinalProject
+{
+    public partial class Cineplex : Form
+    {
+        private OleDbDataAdapter movieDataAdap;
+        private DataSet movieDS;
+        private OleDbCommandBuilder cBuilder;
+        private OleDbConnection dbConn;
+        private OleDbCommand dbCmd;
+
+        public Cineplex()
+        {
+            InitializeComponent();
+        }
+
+        private void Cineplex_Load(object sender, EventArgs e)
+        {
+            dbConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=Movie_Cast_DB.accdb");
+            dbConn.Open();
+
+            this.loadMovies(sender, e);
+            this.hideMovieLabels();
+            this.loadMovieLables(sender, 0);
+        }
+
+        private void hideMovieLabels()
+        {
+            showTime.Visible = false;
+            lblTitle.Visible = false;
+            lblGenre.Visible = false;
+            lblDirector.Visible = false;
+            lblShowTime.Visible = false;
+            lblShowsTime.Visible = false;
+            lblReleaseDate.Visible = false;
+            comboBoxDAY.Visible = false;
+            moviePoster.Image = null;
+            btnDelete.Visible = false;
+            btnUpload.Visible = false;
+            btnUpdate.Visible = true;
+        }
+
+        private void loadMovieLables(object sender, int i)
+        {
+            if (movieDataGrid.SelectedRows[0].Cells[i].Value.ToString() != "")
+            {
+                lblTitle.Visible = true;
+                lblGenre.Visible = true;
+                lblDirector.Visible = true;
+                lblShowTime.Visible = true;
+                lblReleaseDate.Visible = true;
+                comboBoxDAY.Visible = true;
+                btnDelete.Visible = true;
+                btnUpload.Visible = true;
+
+                String dateTxt = movieDataGrid.Rows[i].Cells[3].Value.ToString();
+                DateTime dt = Convert.ToDateTime(dateTxt);
+
+                lblReleaseDate.Text = dt.Date.ToShortDateString();
+                lblTitle.Text = movieDataGrid.Rows[i].Cells[1].Value.ToString();
+                lblGenre.Text = movieDataGrid.Rows[i].Cells[2].Value.ToString();
+                lblDirector.Text = movieDataGrid.Rows[i].Cells[4].Value.ToString();
+                lblShowsTime.Text = movieDataGrid.Rows[i].Cells[5].Value.ToString();
+
+                moviePoster.Image  = null;
+                string imagePath   = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                string picturePath = movieDataGrid.Rows[i].Cells[6].Value.ToString();
+
+                if (picturePath != "")
+                {
+                    imagePath += picturePath;
+                    moviePoster.Image = Image.FromFile(@imagePath);
+                    moviePoster.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                else
+                {
+                    @imagePath += "\\Resources\\no_poster.png";
+                    moviePoster.Image = Image.FromFile(@imagePath);
+                    moviePoster.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+            else
+            {
+                this.hideMovieLabels();
+            }
+        }
+
+        private void loadMovies(object sender, EventArgs e)
+        {
+            try
+            {
+                btnUpdate.Visible = true;
+                btnDelete.Visible = true;
+                btnUpload.Visible = true;
+
+                // Construct an object of the OleDbConnection
+                // class to store the connection string
+                // representing the type of data provider
+                // (database) and the source (actual db)
+                // Construct an object of the OleDbCommand
+                // class to hold the SQL query. 
+                dbCmd = new OleDbCommand();
+                dbCmd.CommandText = "SELECT * FROM movie_information;";
+
+                // Tie the OleDbCommand object to the OleDbConnection object
+                dbCmd.Connection = dbConn;
+
+                movieDataAdap = new OleDbDataAdapter();
+                movieDataAdap.SelectCommand = dbCmd;
+                movieDS = new DataSet();
+                movieDataAdap.Fill(movieDS, "movie_information");
+                movieDataGrid.DataSource = movieDS;
+                movieDataGrid.DataMember = "movie_information";
+                movieDataGrid.Columns[6].Visible = false;
+
+                if (Convert.ToBoolean(movieDataGrid.SelectedRows[0].Cells[0].Value) == true)
+                {
+                    btnUpdate.Visible = true;
+                    btnDelete.Visible = true;
+                    btnUpload.Visible = true;
+                }
+            }
+            catch (System.Exception exc)
+            {
+                lblMessage.Text = exc.Message;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (this.movieDataGrid.SelectedRows.Count == 1)
+            {
+                try
+                {
+                    if (Convert.ToBoolean(movieDataGrid.SelectedRows[0].Cells[0].Value) == true)
+                    {
+                        String selectedMovie = this.movieDataGrid.SelectedRows[0].Cells[0].Value.ToString();
+                        dbCmd.CommandText = "DELETE FROM movie_information WHERE movieId=" + selectedMovie;
+                        dbCmd.ExecuteNonQuery();
+                        this.loadMovies(sender, e);
+                        this.lblMessage.Text = "Movie deleted successfully!!";
+                    }
+                    else
+                    {
+                        MessageBox.Show("There is no data to delete on selected row.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select the row to delete.");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cBuilder = new OleDbCommandBuilder(movieDataAdap);
+                movieDataAdap.Update(movieDS, "movie_information");
+                lblMessage.Text = "Movie is updated successfully....";
+            }
+            catch (System.Exception exc)
+            {
+                this.lblMessage.Text = exc.Message;
+            }
+        }
+
+        private void onMovieClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > -1)
+            {
+                this.hideMovieLabels();
+                this.loadMovieLables(sender, e.RowIndex);
+            }
+        }
+
+        private void comboBoxDAY_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             string[] shows = lblShowsTime.Text.Split(',');
+
+             showTime.Visible = true;
+             if(comboBoxDAY.SelectedIndex < shows.Length) {
+                showTime.Text = shows[comboBoxDAY.SelectedIndex];
+             }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (this.movieDataGrid.SelectedRows.Count == 1)
+            {
+                OpenFileDialog op = new OpenFileDialog();
+                op.ShowDialog();
+                op.Filter = "allfiles|*.png|*.jpg|*.jpeg";
+
+                string[] FName;
+                string imagePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+                if (Convert.ToBoolean(movieDataGrid.SelectedRows[0].Cells[0].Value) == true)
+                {
+                    string selectedMovieID = movieDataGrid.SelectedRows[0].Cells[0].Value.ToString();
+
+                    foreach (string s in op.FileNames)
+                    {
+                        try
+                        {
+                            FName = s.Split('\\');
+                            string targetURL = "\\Resources\\" + FName[FName.Length - 1];
+                            imagePath += targetURL;
+
+                            if (File.Exists(@imagePath))
+                            {
+                                File.Delete(@imagePath);
+                            }
+
+                            File.Copy(s, imagePath, true);
+                            dbCmd.CommandText = "UPDATE movie_information SET posturePath = '" + targetURL + "' WHERE movieId=" + selectedMovieID;
+                            dbCmd.ExecuteNonQuery();
+
+                            moviePoster.Image = Image.FromFile(@imagePath);
+                            moviePoster.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                            this.loadMovies(sender, e);
+                            this.lblMessage.Text = "Movie Poster Updated Successfully!!";
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("You can not upload the same image.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
